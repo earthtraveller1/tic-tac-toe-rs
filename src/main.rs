43a2235647b8778ed440b143aec39d42429c98a3
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-01-15 19:51:58 -0500

    Created a basic window.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::{
+    conf::WindowSetup,
+    event::{self, EventHandler},
+    ContextBuilder, GameError, GameResult,
+};
+
+struct Game {}
+
+impl Game {
+    fn new() -> GameResult<Game> {
+        Ok(Game {})
+    }
+}
+
+impl EventHandler<GameError> for Game {
+    fn update(&mut self, _ctx: &mut ggez::Context) -> Result<(), GameError> {
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut ggez::Context) -> Result<(), GameError> {
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let (context, event_loop) = ContextBuilder::new("tic-tac-toe", "earthtraveller1")
+        .window_setup(WindowSetup {
+            title: "Tic Tac Toe".to_string(),
+            ..Default::default()
+        })
+        .build()?;
+
+    let game = Game::new()?;
+    event::run(context, event_loop, game)
 }
